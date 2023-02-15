381e6c979124f40c34d615df8d9ada16763da5cd
josemejias081 <josemejias081@gmail.com>
2023-02-15 23:00:12 +0100

    denied access to users has role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
+  
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => "No tienes permiso para ejecutar esa acción"
+  end 
+
+  
 end
--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,4 +1,5 @@
 class BusinessesController < ApplicationController
+  load_and_authorize_resource
   before_action :set_business, only: [ :show, :edit, :update, :destroy ]
 
   def index
