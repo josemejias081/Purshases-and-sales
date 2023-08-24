d1057ac1fd42daa72684d480b29b028d00f819b6
josemejias081 <josemejias081@gmail.com>
2023-08-24 09:13:07 +0200

    added validates to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   has_many :businesses, dependent: :destroy
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :icon, presence: true
+  validates :photo, presence: true
 end
