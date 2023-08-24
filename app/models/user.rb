8821c1ed4ce5e216d531f227bc7a709ac5b0ed67
josemejias081 <josemejias081@gmail.com>
2023-08-24 09:33:11 +0200

    added validates to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :role, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+  validates :password, presence: true
 
   has_many :businesses, dependent: :destroy
 
